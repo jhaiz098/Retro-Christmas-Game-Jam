4e40517ab164de52aecf225e843a5121387dd3b5
James <james.fernandez1239@gmail.com>
2024-12-14 20:58:39 +0800

    1

--- Retro Christmas Game Jam/Assets/_Scripts/BonFire.cs
+++ Retro Christmas Game Jam/Assets/_Scripts/BonFire.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class BonFire : MonoBehaviour
 {
+    [SerializeField] private float playerDetectionRange;
+    [SerializeField] private LayerMask playerMask;
     private bool burning = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,11 +14,24 @@ public class BonFire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Collider2D playerDetector = Physics2D.OverlapCircle(transform.position, playerDetectionRange, playerMask);
+
+        if (playerDetector != null)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SetFire();
+            }
+        }
     }
 
     public void SetFire()
     {
         burning = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, playerDetectionRange);
     }
 }
--- Retro Christmas Game Jam/Assets/_Scripts/Player.cs
+++ Retro Christmas Game Jam/Assets/_Scripts/Player.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    private Vector2 movementInput;
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        rb.linearVelocity = new Vector2(movementInput.x * speed, movementInput.y * speed);
     }
 }
