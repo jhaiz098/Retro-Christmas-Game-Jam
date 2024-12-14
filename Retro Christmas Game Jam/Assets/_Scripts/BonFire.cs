e44ef1056b942e501d9d4f69df62ed6b81931bf0
James <james.fernandez1239@gmail.com>
2024-12-14 19:26:33 +0800

    Added placeholder bon fire and player

---
+++ Retro Christmas Game Jam/Assets/_Scripts/BonFire.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BonFire : MonoBehaviour
+{
+    private bool burning = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetFire()
+    {
+        burning = true;
+    }
+}
